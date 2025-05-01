7800b4408e41461029bbda3fceb7f573323dca03
cmoney4200 <82184215+cmoney4200@users.noreply.github.com>
2025-05-01 00:51:57 -0400

    Update buff_overflow_bad_t2.c

--- Buffer_Overflow_Transformations/buff_overflow_bad_t2.c
+++ Buffer_Overflow_Transformations/buff_overflow_bad_t2.c
@@ -3,12 +3,13 @@
 void vulnerable_copy(char* input, int unused_param) 
 {
     char buffer[12];
-    strcpy(buffer, input);  // Original parameter order (required for vulnerability)
+    //Bounds not checked, vulnerable
+    strcpy(buffer, input);
 }
 
 int main(int argc, char** argv) 
 {
     char large_input[128] = "This string is definitely longer than 12 characters";
-    vulnerable_copy(large_input, 0);  // Added unused parameter
+    vulnerable_copy(large_input, 0);
     return 0;
 }
