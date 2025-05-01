4a761f10c0a9f5a74d96c20a082d63d2a4869460
cmoney4200 <82184215+cmoney4200@users.noreply.github.com>
2025-05-01 00:52:53 -0400

    Update buff_overflow_bad_t3.c

--- Buffer_Overflow_Transformations/buff_overflow_bad_t3.c
+++ Buffer_Overflow_Transformations/buff_overflow_bad_t3.c
@@ -1,13 +1,13 @@
 #include <string.h>
 
-/* Renamed functions */
-void unsafe_memory_operation(char* source_data) {  // Previously vulnerable_copy
+void unsafe_memory_operation(char* source_data) { 
     char small_buffer[12];
-    strcpy(small_buffer, source_data);  // Same vulnerability
+    //Bounds not checked, vulnerable
+    strcpy(small_buffer, source_data);
 }
 
-int program_entry() {  // Previously main
-    char oversized_data[128] = "This string is definitely longer than 64 characters...";
+int program_entry() {
+    char oversized_data[128] = "This string is definitely longer than 12 characters...";
     unsafe_memory_operation(oversized_data);
     return 0;
 }
