676f8e65561e055970d17b6e5dca05d9d84af94f
cmoney4200 <82184215+cmoney4200@users.noreply.github.com>
2025-05-01 00:12:01 -0400

    Update buff_overflow_good.c

--- Good_DataSets/buff_overflow_good.c
+++ Good_DataSets/buff_overflow_good.c
@@ -1,14 +1,14 @@
 #include <string.h>
 
 void safe_copy(const char* input) {
-    char buffer[64];
+    char buffer[12];
     //Bounds safely checked
     strncpy(buffer, input, sizeof(buffer) - 1);
     buffer[sizeof(buffer) - 1] = '\0';
 }
 
 int main() {
-    char large_input[128] = "This string is longer but safely handled";
+    char large_input[128] = "This string is definitely longer than 12 characters...;
     safe_copy(large_input);
     return 0;
 }
