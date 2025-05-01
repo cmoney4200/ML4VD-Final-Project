3009cef734532905225d97f40e0c5ddddbceef28
cmoney4200 <82184215+cmoney4200@users.noreply.github.com>
2025-05-01 00:10:41 -0400

    Update buff_overflow_bad.c

--- Buffer_Overflow_Transformations/buff_overflow_bad.c
+++ Buffer_Overflow_Transformations/buff_overflow_bad.c
@@ -2,11 +2,12 @@
 
 void vulnerable_copy(char* input) {
     char buffer[12];
-    strcpy(buffer, input);  // No bounds checking
+    //Bounds not checked, vulnerable
+    strcpy(buffer, input);
 }
 
 int main() {
-    char large_input[128] = "This string is definitely longer than 64 characters...";
+    char large_input[128] = "This string is definitely longer than 12 characters...";
     vulnerable_copy(large_input);
     return 0;
 }
